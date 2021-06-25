8c2383c81db0bc2369608b02ecb5f58b7dada984
Muhammadqodir <m.qodir777@gmail.com>
2021-06-26 00:03:31 +0300

    Added launcher icon & bottom bar & colors

--- app/src/main/java/uz/mq/focus/MainActivity.kt
+++ app/src/main/java/uz/mq/focus/MainActivity.kt
@@ -2,10 +2,19 @@ package uz.mq.focus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBar()
     }
+
+    private fun setActionBar() {
+        supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        supportActionBar?.setCustomView(R.layout.action_bar);
+
+    }
+
 }
