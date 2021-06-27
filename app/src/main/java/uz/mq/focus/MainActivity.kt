8ba617b2c33a5f24bbb3b1596f5978f1028121d5
Muhammadqodir <m.qodir777@gmail.com>
2021-06-27 04:29:52 +0300

    Added menu

--- app/src/main/java/uz/mq/focus/MainActivity.kt
+++ app/src/main/java/uz/mq/focus/MainActivity.kt
@@ -3,6 +3,8 @@ package uz.mq.focus
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.navigation.NavController
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var bottomBar: SmoothBottomBar
     lateinit var context: Context
     lateinit var navController: NavController
+    lateinit var addTask: MenuItem
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,18 +30,40 @@ class MainActivity : AppCompatActivity() {
     private fun setActionBar() {
         supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
         supportActionBar?.setCustomView(R.layout.action_bar);
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_icons8_settings)
     }
 
     private fun initViews(){
         navController = findNavController(R.id.fragment)
         bottomBar = findViewById(R.id.bottomBar)
         bottomBar.setOnItemSelectedListener {
-            when(it){
-                0 -> navController.navigate(R.id.todayFragment)
-                1 -> navController.navigate(R.id.tasksFragment)
-                2 -> navController.navigate(R.id.walletFragment)
-            }
+            showFragment(it)
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_actionbar, menu)
+        addTask = menu!!.findItem(R.id.addTask)
+        addTask.setVisible(false)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    fun showFragment(index: Int): Unit{
+        when(index){
+            0 -> {
+                addTask.setVisible(false)
+                navController.navigate(R.id.todayFragment)
+            }
+            1 -> {
+                addTask.setVisible(true)
+                navController.navigate(R.id.tasksFragment)
+            }
+            2 -> {
+                addTask.setVisible(false)
+                navController.navigate(R.id.walletFragment)
+            }
+        }
+    }
 }
